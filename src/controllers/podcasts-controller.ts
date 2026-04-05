9b6db0c57b8c1a00fb849966dc95e6f61f18a0d2
matheus-cmc <matheusfeira@hotmail.com>
2026-04-05 17:10:00 -0300

    atualização

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -18,7 +18,10 @@ export const getFilterEpisodes = async (
   req: IncomingMessage,
   res: ServerResponse
 )=>{
- const content = await servicesFilterEpisodes("flow");
+
+  const queryString =req.url ?.split("?p=")[1] ?? "";
+
+ const content = await servicesFilterEpisodes(queryString);
 
    res.writeHead(200, { "Content-Type": "application/json" });
    res.end(JSON.stringify(content));
